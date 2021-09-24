05aa43991ad7b59e586793c4a270a7e559d4c65f
RonGrego <l00163553@student.lyit.ie>
2021-09-24 14:54:57 +0530

    initial commit

---
+++ tut1_pkg/q1.py
@@ -0,0 +1,34 @@
+print("\t"*2+"Title")
+# add two tab space before title
+# Output -      Title
+
+print("Hello")
+# print statement is used to print anything in the console
+# Output - Hello
+
+print('Hello World')
+# inside print statement we can use either double quotes or single quotes
+# Output - Hello World
+
+a=5
+# assign integer value to variable a
+
+b=12.3456789
+# assign a float value to variable b
+
+c=a+b
+# add a and b and result stores in variable c
+
+d="A number"+b
+
+e=a+ \
+    b+ \
+    c
+f=2
+g=a/f
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
