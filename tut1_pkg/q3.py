ca9b335171fc4ef10644d26d84fd7f63377d69aa
RonGrego <l00163553@student.lyit.ie>
2021-09-24 15:53:02 +0530

    Question 3

---
+++ tut1_pkg/q3.py
@@ -0,0 +1,10 @@
+
+# . ppsn # String because its a collection of number and characters
+# . passport number # String because its a collection of number and characters
+# . temperature in degrees # float because temperature can be in decimal
+# . Miles travelled per day # float
+# . average collection of exam grades # Arrays/list because we can use these datatype to store collections
+# . file path # String
+# . persons middle initial # String
+# . Book abstract # String
+# . Mathematical function # String
