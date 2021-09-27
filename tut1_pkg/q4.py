1d0a60218fb12ef6c0b3dad5c46a833747d0abe3
RonGrego <l00163553@student.lyit.ie>
2021-09-27 12:20:40 +0530

    added question 4

---
+++ tut1_pkg/q4.py
@@ -0,0 +1,22 @@
+
+
+ans_1 = 6 + 8 / 2 - 1
+# Divides the number on its left by the number on its right and returns a floating point value
+print(ans_1)
+ans_2 = 1 + 1 - 1 + 2 / 2
+ans_3 = 1.1 + 5 / 2
+ans_4 = 9.2 / 3
+# Dividing a float by an integer returns a float value
+ans_5 = 9.2 % 3
+#  % operator returns the remainder of dividing the left hand operand by right hand operand
+ans_6 = 1 * 2 + 3 * 5 % 4
+ans_7 = 1 + 8 % 3 * 2 - 9
+ans_8 = 6 + ( 24 % (16 - 11))
+
+print(ans_2)
+print(ans_3)
+print(ans_4)
+print(ans_5)
+print(ans_6)
+print(ans_7)
+print(ans_8)
